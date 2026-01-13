ddb39113f63b9bbcf14efa58cd6d2f628cf6b735
StefanoFaiola <stefano.faiola@outlook.com>
2026-01-13 00:52:08 +0000

    Player creation from circleshape class, inherit from Pygame sprite)

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
+
 
 def main():
-    print(f"""Starting Asteroids with pygame version: {pygame.version.ver}
-            Screen width: {SCREEN_WIDTH} 
-            Screen height: {SCREEN_HEIGHT}"""
-            )
-    
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(x,y)
+    dt = 0
+
 
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        #Limit timeframe to 60 fps
+        dt = clock.tick(60) /1000
 
 
 if __name__ == "__main__":
