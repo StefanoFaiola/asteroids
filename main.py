0b45dee4099786814e1e1cd1079a1eb7d05181a1
StefanoFaiola <stefano.faiola@outlook.com>
2026-01-13 01:15:20 +0000

    Making players able to move creating a move function used in update when w and s are pressed

--- main.py
+++ main.py
@@ -18,16 +18,20 @@ def main():
     dt = 0
 
 
+
     while True:
         log_state()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
 
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+       
 
         #Limit timeframe to 60 fps
         dt = clock.tick(60) /1000
